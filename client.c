ff1b4fab6d1e023a8e92e7695eef6366f4ad94f5
Divyanshu844 <157150450+Divyanshu844@users.noreply.github.com>
2025-06-26 00:41:14 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,77 +33,112 @@ void menu(SOCKET sock, char *username) {
         printf("3. Book Seat\n");
         printf("4. Delete My Ride\n");
         printf("5. View My Ride History\n");
-        printf("6. Exit\n");
+        printf("6. View Number of Rides Booked\n"); // New option added
+        printf("7. Exit\n"); // Exit option moved
         printf("Enter choice: ");
         scanf("%d", &choice);
         getchar();
 
         switch (choice) {
-        case 1: {
-            char ride_id[20], origin[50], destination[50], vehicle[50];
-            int seats;
-            float distance, time;
+            case 1: {
+                char ride_id[20], origin[50], destination[50], vehicle[50];
+                int seats;
+                float distance, time;
 
-            printf("Enter Ride ID: ");
-            gets(ride_id);
-            printf("Origin: ");
-            gets(origin);
-            printf("Destination: ");
-            gets(destination);
-            printf("Seats: ");
-            scanf("%d", &seats); getchar();
-            printf("Distance (km): ");
-            scanf("%f", &distance); getchar();
-            printf("Time (mins): ");
-            scanf("%f", &time); getchar();
-            printf("Vehicle Type: ");
-            gets(vehicle);
+                printf("Enter Ride ID: ");
+                gets(ride_id);
+                printf("Origin: ");
+                gets(origin);
+                printf("Destination: ");
+                gets(destination);
+                printf("Seats: ");
+                scanf("%d", &seats); getchar();
+                printf("Distance (km): ");
+                scanf("%f", &distance); getchar();
+                printf("Time (mins): ");
+                scanf("%f", &time); getchar();
+                printf("Vehicle Type: ");
+                gets(vehicle);
 
-            sprintf(buffer, "CREATE_RIDE|%s|%s|%s|%s|%d|%.2f|%.2f|%s",
-                    ride_id, username, origin, destination, seats, distance, time, vehicle);
-            send_request(sock, buffer);
-            receive_response(sock);
-            break;
-        }
-        case 2: {
-            char keyword[50];
-            printf("Enter keyword (origin, destination, or ride ID): ");
-            gets(keyword);
-            sprintf(buffer, "SEARCH|%s", keyword);
-            send_request(sock, buffer);
-            receive_response(sock);
-            break;
-        }
-        case 3: {
-            char ride_id[20];
-            printf("Enter Ride ID to book: ");
-            gets(ride_id);
-            sprintf(buffer, "BOOK|%s|%s", ride_id, username);
-            send_request(sock, buffer);
-            receive_response(sock);
-            break;
-        }
-        case 4: {
-            char ride_id[20];
-            printf("Enter Ride ID to delete: ");
-            gets(ride_id);
-            sprintf(buffer, "DELETE|%s|%s", ride_id, username);
-            send_request(sock, buffer);
-            receive_response(sock);
-            break;
-        }
-        case 5:
-            sprintf(buffer, "HISTORY|%s", username);
-            send_request(sock, buffer);
-            receive_response(sock);
-            break;
-        case 6:
-            closesocket(sock);
-            WSACleanup();
-            printf("Disconnected.\n");
-            return;
-        default:
-            printf("Invalid choice.\n");
+                sprintf(buffer, "CREATE_RIDE|%s|%s|%s|%s|%d|%.2f|%.2f|%s",
+                        ride_id, username, origin, destination, seats, distance, time, vehicle);
+                send_request(sock, buffer);
+                receive_response(sock);
+                break;
+            }
+            case 2: {
+                char keyword[50];
+                printf("Enter keyword (origin, destination, or ride ID): ");
+                gets(keyword);
+                sprintf(buffer, "SEARCH|%s", keyword);
+                send_request(sock, buffer);
+                receive_response(sock);
+                break;
+            }
+            case 3: {
+                char ride_id[20];
+                int seats, total_seats;
+
+                printf("Enter Ride ID to book: ");
+                fgets(ride_id, sizeof(ride_id), stdin);
+                ride_id[strcspn(ride_id, "\n")] = 0;
+
+
+                sprintf(buffer, "GET_TOTAL_SEATS|%s", ride_id);
+                send_request(sock, buffer);
+                // Receive the total seats response here
+                char total_seats_response[BUFFER_SIZE];
+                int bytes_received = recv(sock, total_seats_response, sizeof(total_seats_response) - 1, 0);
+                if (bytes_received > 0) {
+                    total_seats_response[bytes_received] = '\0';
+                    sscanf(total_seats_response, "TOTAL_SEATS|%d", &total_seats);
+                    printf("Total seats available: %d\n", total_seats);
+                } else {
+                    printf("Could not get total seats for ride.\n");
+                    break;
+                }
+
+                printf("Enter number of seats to book: ");
+                scanf("%d", &seats);
+                getchar();
+
+                if (seats <= 0 || seats > total_seats) {
+                    printf("Invalid number of seats.\n");
+                } else {
+                    sprintf(buffer, "BOOK|%s|%s|%d", ride_id, username, seats);
+                    send_request(sock, buffer);
+                    receive_response(sock);
+
+                }
+                break;
+            }
+            case 4: {
+                char ride_id[20];
+                printf("Enter Ride ID to delete: ");
+                gets(ride_id);
+                sprintf(buffer, "DELETE|%s|%s", ride_id, username);
+                send_request(sock, buffer);
+                receive_response(sock);
+                break;
+            }
+            case 5:
+                sprintf(buffer, "HISTORY|%s", username);
+                send_request(sock, buffer);
+                receive_response(sock);
+                break;
+            case 6: {
+                sprintf(buffer, "GET_BOOKED_RIDES_COUNT|%s", username);
+                send_request(sock, buffer);
+                receive_response(sock);
+                break;
+            }
+            case 7: // Exit option
+                closesocket(sock);
+                WSACleanup();
+                printf("Disconnected.\n");
+                return;
+            default:
+                printf("Invalid choice.\n");
         }
     }
 }
